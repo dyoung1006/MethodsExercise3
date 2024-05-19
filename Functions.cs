bdf8a2c048099a8ca64b60aa791af70f8703130b
Damien Young <dyoung1006@gmail.com>
2024-05-19 13:29:29 -0700

    Add project files.

---
+++ Functions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterationStatements2024
+{
+    public static class Display
+    {        
+        public static void PrintNumbers(int startNum, int endNum)
+        {
+            while (startNum >= endNum)
+            {
+                Console.WriteLine($"{startNum}");
+                startNum--;
+            }
+        }        
+        public static void Three(int startNum, int endNum)
+        {
+            for (int i = startNum; i <= endNum; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void EqualityCheck(int startNum, int endNum)
+        {                                
+            Console.Write($"The numbers provided ({startNum},{endNum}) are equal: {Functions.IsMatch(startNum, endNum)}");
+        }
+        public static void MultiplicatonTable(int num)
+        {
+            for (var i = 0; i <= 12; i++)
+            {
+                Console.WriteLine($"{num} x {i} = {num * i}");
+            }
+        }
+    }
+    public static class Functions
+    {
+        //Write a method to accept two integers as parameterss and check whether they are equal or not
+        public static bool IsMatch(int startNum, int endNum)
+        {
+            return startNum == endNum;
+        }
+
+        //Write a method to check whether a given number is even or odd
+        public static bool IsEven(int num)
+        {
+            return num % 2 == 0;
+        }
+
+        //Write a method to check whether a given number is positive or negative
+        public static bool IsPositive(int num)
+        {
+            return num >= 0;
+        }
+
+        //Write a method to read the age of a candidate and determine whether they can vote.
+        public static bool VoterEligible(int num)
+        {
+            return num >= 18;
+        }       
+        public static bool IsInRange(int firstNum, int secondNum, int givenNum)
+        {                   
+            return (givenNum >= firstNum && givenNum <= secondNum) ? true : false;
+        }        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace IterationStatements2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var someNumber = 1;
+
+            //Write a method that will print to the console all numbers 1000 through - 1000
+            //Display.PrintNumbers(1000,-1000);
+
+            //Write a method that will print to the console numbers 3 through 999 by 3 each time
+            //Display.Three(3, 999);
+
+            //Write a method to accept two integers as parameters and check whether they are equal or not
+            //Display.EqualityCheck(1, 1);
+
+            //Write a method to check whether a given number is even or odd            
+            //if (Functions.IsEven(someNumber))
+            //{
+            //    Console.WriteLine($"The given number {someNumber}, is True");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"The given number {someNumber}, is False");
+            //}
+
+            //Write a method to check whether a given number is positive or negative            
+            //if (Functions.IsPositive(someNumber))
+            //{
+            //    Console.WriteLine($"The given number {someNumber}, is positive");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine($"The given number {someNumber}, is negative");
+            //}
+
+            //Write a method to read the age of a candidate and determine whether they can vote.
+            //if (Functions.VoterEligible(someNumber))
+            //{
+            //    Console.WriteLine($"Given the age of {someNumber}, you are eligble to vote");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Given the age of {someNumber}, you are NOT eligble to vote");
+            //}
+
+            //Write a method to check if an integer (from the user) is in the range -10 to 10
+            //Console.WriteLine($"The given number of {someNumber}, is between -10 and 10 is True: { Functions.IsInRange(-10, 10, someNumber)}");
+
+            //Write a method to display the multiplication table (from 1 to 12) of a given integer
+            //Display.MultiplicatonTable(someNumber);
+            Console.ReadLine();
+        }
+    }
+}
